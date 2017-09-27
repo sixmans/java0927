a3d038da8df97832bc7077e242d5af55d765ce9e
sixmans <sixmans153624@gmail.com>
2017-09-27 20:13:20 +0800

    aaa

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("106024267 黃子儀");
-        System.out.println("106024267");// write your code here
+        System.out.println("106024267");
+        System.out.println("Asia Universityl");// write your code here
     }
 }
